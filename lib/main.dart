473f0c9c22c094d2e30a9da32978aa91b71c1148
Vivek Shah <bulkminds@gmail.com>
2021-02-21 18:48:07 +0530

    drawer update

--- lib/main.dart
+++ lib/main.dart
@@ -12,32 +12,31 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("My demo"),
+        backgroundColor: Colors.purple,
       ),
-      body: Container(
-        color: Colors.black,
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height / 2,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
+      body: Container(),
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            Container(
-              color: Colors.pink,
-              width: 100,
-              alignment: Alignment.center,
-              height: 100,
+            UserAccountsDrawerHeader(
+              accountName: Text("android"),
+              accountEmail: Text("androidsChef@gmail.com"),
+              decoration: BoxDecoration(color: Colors.purple),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage("http://t1.gstatic.com/images?q=tbn:ANd9GcShJe_TpVKYx0LvNVr-GLQpUm-qc86h5xPNacq2A8GiYgOtNBILTkINjh7jqYe6"),
+              ),
             ),
-            Container(
-              color: Colors.grey,
-              width: 100,
-              alignment: Alignment.center,
-              height: 100,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Sub title"),
+              trailing: Icon(Icons.edit),
             ),
-            Container(
-              color: Colors.red,
-              width: 100,
-              alignment: Alignment.center,
-              height: 100,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Account"),
+              subtitle: Text("Sub title"),
+              trailing: Icon(Icons.edit),
             ),
           ],
         ),
